643541b2d7cf1f31fe561e10a8cc342e2bed5e30
Awet Tesfay <whitezscope@gmail.com>
2020-08-24 14:03:38 +0200

    make calculator using switch

--- 02-maths/02-calculator-two/script.js
+++ 02-maths/02-calculator-two/script.js
@@ -11,9 +11,25 @@
 
 (function() {
     // to get the value of an input: document.getElementById("element-id").value
+    let v1 = parseFloat(document.getElementById("op-one").value);
+    let v2 = parseFloat(document.getElementById("op-two").value);
 
-    var performOperation = function(operation) {
+    const performOperation = function(operation) {
         // perform the operation
+        switch (operation) {
+            case "addition":
+                alert(v1 + v2);
+                break;
+            case "subtraction":
+                alert(v1 - v2);
+                break;
+            case "multiplication":
+                alert(v1 * v2);
+                break;
+            case "division":
+                alert(v1 / v2);
+                break;
+        }
     };
 
     Array.from(document.querySelectorAll("button.operator")).forEach(function($btn) {
