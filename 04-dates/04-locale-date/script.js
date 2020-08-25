a59f6d2ce1b060bdba0be469b48b9c00a807caf9
Awet Tesfay <whitezscope@gmail.com>
2020-08-25 11:30:37 +0200

    create time display function

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -12,7 +12,16 @@
 (function() {
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
-
     // your code here
+    let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    weekdays.forEach(function () {
+        let now = new Date();
+        let days = weekdays[now.getDay()];
+        let yearmonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        yearmonth.forEach(function () {
+            let months = yearmonth[now.getMonth()];
+            document.getElementById("target").innerHTML = `${days} ${now.getDate()} ${months} ${now.getFullYear()} ${now.getHours()}h${now.getMinutes()}`;
+        })
+    })
 
 })();
