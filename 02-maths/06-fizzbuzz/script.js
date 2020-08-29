3fab8928fbf496ef68b0eb6109d1e77c4d6c0114
Awet Tesfay <whitezscope@gmail.com>
2020-08-29 15:06:21 +0200

    remove unwanted paranthesis

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -13,7 +13,7 @@
 
     // your code here
     for (let i = 1; i <= 100; i++) {
-        if ((i % 3 === 0) && (i % 5 === 0)) {
+        if (i % 3 === 0 && i % 5 === 0) {
             console.log("fizzbuzz");
         } else if (i % 3 === 0) {
             console.log("buzz");
