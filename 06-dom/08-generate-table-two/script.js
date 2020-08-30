525cd292fd1d607c46b31605a6c2bb0fd984db23
Awet Tesfay <whitezscope@gmail.com>
2020-08-30 12:45:53 +0200

    remove the extra loop and make it more readable

--- 06-dom/08-generate-table-two/script.js
+++ 06-dom/08-generate-table-two/script.js
@@ -13,42 +13,41 @@
 
     function tableRow() {
 
+        // select the target and create table and tbody elements
         const target = document.querySelector("#target");
-        let table = document.createElement("table");
-        let tbody = document.createElement('tbody');
+        const table = document.createElement("table");
+        const tbody = document.createElement('tbody');
 
+        // create empty array for the table data and for multiplication
+        const tdArray = [];
+        const multiplication = [];
 
-        let tdArray = [];
-        let td;
+        // loop to create 10 table rows each with 10 columns manipulate the multiplication operation
         for (let i = 0; i < 10; i++) {
-            let tr = document.createElement('tr');
+            const tr = document.createElement('tr');
             for (let j = 0; j < 10; j++) {
-                td = document.createElement('td');
+                const td = document.createElement('td');
+
+                // append the table data to the table rows and push it to the tdArray
                 tr.appendChild(td);
                 tdArray.push(td);
+
+                // make the multiplication operation and push result to the multiplication array
+                const num = (i+1) * (j+1);
+                multiplication.push(num);
+
             }
+            // append table rows to the tbody
             tbody.appendChild(tr);
         }
-
+        // append tbody to table and table to target
         table.appendChild(tbody);
         target.appendChild(table);
 
-
-        let multiplication = [];
-        let num;
-        for (let i = 1; i < 11; i++) {
-            for (let j = 1; j < 11; j++) {
-                num = i * j;
-                multiplication.push(num);
-            }
-        }
-
-
+        // place the result of the multiplication to the respective position in the table
         multiplication.forEach(($mun, index)=> {
             tdArray[index].innerHTML = multiplication[index];
         })
-
-
     }
 
     tableRow();
