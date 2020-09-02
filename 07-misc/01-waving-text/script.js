3c78658aa9505987b48627cbe1608cb7b8166d1c
Awet Tesfay <whitezscope@gmail.com>
2020-09-02 15:32:34 +0200

    updata by making the fonsize variable each time

--- 07-misc/01-waving-text/script.js
+++ 07-misc/01-waving-text/script.js
@@ -12,32 +12,39 @@
 (function() {
 
 
+    // select the target and set text to it
     let target = document.querySelector("#target");
     target.innerHTML = "Create wave effect to the text using font-size.";
 
-
+    // create empty array for the newly created spans
     let spanArray = [];
 
-    Array.from(target.textContent).forEach(($letter, index) => {
-        let span = document.createElement("span");
-        span.innerHTML = $letter;
+    // create array from the text content of the target
+    Array.from(target.textContent).forEach($letter => {
+        let span = document.createElement("span"); // create spans equivalent to the number of letters
+        span.innerHTML = $letter; // update each span with the letter of the array
         spanArray.push(span);
     });
 
+    // make the target empty, b/c we will add spans children to it later
     target.innerHTML = "";
 
-    spanArray.forEach(($span, index)=> {
 
-        let fontSize;
-        for (let i = 0; i < 6; i++) {
-            fontSize = Math.floor(Math.random()*6 + 1);
-        }
-        target.appendChild($span);
+    setInterval(() => {
+        spanArray.forEach(($span, index)=> {
+            // put the fontSize variable out b/c will be used out of the for loop scope
+            let fontSize;
+            // generate random font size b/n 1 and 6
+            for (let i = 0; i < 6; i++) {
+                fontSize = Math.floor(Math.random()*6 + 1);
+            }
+            // append each span to the target
+            target.appendChild($span);
+            // give fontsize style to the span.
+            spanArray[index].style.fontSize = `${fontSize}rem`;
 
-        spanArray[index].style.fontSize = `${fontSize}rem`;
-
-
-    });
+        });
+    }, 300);
 
 
 })();
