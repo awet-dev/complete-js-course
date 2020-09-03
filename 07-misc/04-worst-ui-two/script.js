2f576486de621ec44da9cf76465ce97e1188c2c4
Awet Tesfay <whitezscope@gmail.com>
2020-09-03 10:05:03 +0200

    shorten the code and make it readabel

--- 07-misc/04-worst-ui-two/script.js
+++ 07-misc/04-worst-ui-two/script.js
@@ -27,49 +27,22 @@
         btnMaxArray.push(inputMax);
     });
 
-    // listen to the first btn click and update the target and its btn value and the data-min as wel.
-    buttons[0].addEventListener("click", ()=> {
-        if (btnMinArray[0] < btnMaxArray[0]) {
-            btnMinArray[0]++;
-            buttons[0].innerHTML = btnMinArray[0];
-            target.innerHTML = `0${btnMinArray[0]} ${btnMinArray[1]} ${btnMinArray[2]} ${btnMinArray[3]}`;
-        }
-    });
+    // loop through the buttons to listen to your click event
+    buttons.forEach((btn, index)=> {
+        btn.addEventListener("click", ()=> {
 
-    // listen to the 2nd btn click and update the target and its btn value and the data-min as wel.
-    buttons[1].addEventListener("click", ()=> {
-        if (btnMinArray[1] < btnMaxArray[1]) {
-            btnMinArray[1]++;
-            if (btnMinArray[1] < 10) {
-                btnMinArray[1] = `0${btnMinArray[1]}`
-            }
-            buttons[1].innerHTML = btnMinArray[1];
-            target.innerHTML = `0${btnMinArray[0]} ${btnMinArray[1]} ${btnMinArray[2]} ${btnMinArray[3]}`;
-        }
-    });
+            // check if the  data-min is still less than data-max
+            if (btnMinArray[index] < btnMaxArray[index]) {
+                btnMinArray[index]++;
 
-    // listen to the 3rd btn click and update the target and its btn value and the data-min as wel.
-    buttons[2].addEventListener("click", ()=> {
-        if (btnMinArray[2] < btnMaxArray[2]) {
-            btnMinArray[2]++;
-            if (btnMinArray[2] < 10) {
-                btnMinArray[2] = `0${btnMinArray[2]}`
+                // check if data-min is less then ten, if so add zero in front of it
+                if (btnMinArray[index] < 10) {
+                    btnMinArray[index] = `0${btnMinArray[index]}`
+                }
+                buttons[index].innerHTML = btnMinArray[index];
+                target.innerHTML = `0${btnMinArray[0]} ${btnMinArray[1]} ${btnMinArray[2]} ${btnMinArray[3]}`;
             }
-            buttons[2].innerHTML = btnMinArray[2];
-            target.innerHTML = `0${btnMinArray[0]} ${btnMinArray[1]} ${btnMinArray[2]} ${btnMinArray[3]}`;
-        }
-    });
-
-    // listen to the 4th btn click and update the target and its btn value and the data-min as wel.
-    buttons[3].addEventListener("click", ()=> {
-        if (btnMinArray[3] < btnMaxArray[3]) {
-            btnMinArray[3]++;
-            if (btnMinArray[3] < 10) {
-                btnMinArray[3] = `0${btnMinArray[3]}`
-            }
-            buttons[3].innerHTML = btnMinArray[3];
-            target.innerHTML = `0${btnMinArray[0]} ${btnMinArray[1]} ${btnMinArray[2]} ${btnMinArray[3]}`;
-        }
+        })
     });
 
 })();
