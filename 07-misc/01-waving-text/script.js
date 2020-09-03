662547035e83334882ae4e739ff9d98247d96e8c
Awet Tesfay <whitezscope@gmail.com>
2020-09-03 08:37:24 +0200

    modify the animation

--- 07-misc/01-waving-text/script.js
+++ 07-misc/01-waving-text/script.js
@@ -21,7 +21,7 @@
 
     // create array from the text content of the target
     Array.from(target.textContent).forEach($letter => {
-        let span = document.createElement("span"); // create spans equivalent to the number of letters
+        const span = document.createElement("span"); // create spans equivalent to the number of letters
         span.innerHTML = $letter; // update each span with the letter of the array
         spanArray.push(span);
     });
@@ -29,22 +29,26 @@
     // make the target empty, b/c we will add spans children to it later
     target.innerHTML = "";
 
-
+    // initiate the font size with 20px
+    let fontSize = 20;
     setInterval(() => {
         spanArray.forEach(($span, index)=> {
-            // put the fontSize variable out b/c will be used out of the for loop scope
-            let fontSize;
-            // generate random font size b/n 1 and 6
-            for (let i = 0; i < 6; i++) {
-                fontSize = Math.floor(Math.random()*6 + 1);
+
+            // keep the font size between 20 and 30 px
+            if (fontSize > 30) {
+                fontSize = 20;
             }
+
             // append each span to the target
             target.appendChild($span);
-            // give fontsize style to the span.
-            spanArray[index].style.fontSize = `${fontSize}rem`;
+
+            // give fontsize style to the span and increment fontsize by 3.
+            $span.style.fontSize = `${fontSize}px`;
+            fontSize += 3;
 
         });
-    }, 300);
+
+    }, 500);
 
 
 })();
